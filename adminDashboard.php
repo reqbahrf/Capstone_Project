a9693ce47b804b764228ce17cb74f04567fbe103
Reanz Arthur <reqbahrf@gmail.com>
2024-04-17 09:31:28 +0800

    {addition of admin dashboard and inprovement of navbars}

---
+++ adminDashboard.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+  <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <style>
+    .scrollable-main {
+      overflow-y: auto;
+    }
+  </style>
+</head>
+<body class="overflow-hidden">
+  <div class="container-fluid px-0">
+      <div class="d-flex align-items-center bg-primary">
+        <img src="./assets/img/74px-DOST_seal.svg.png" alt="DOST logo" class="me-3 ms-3 mt-3 mb-3" style="width: 30px; height: 30px; border-radius: 25%; border: 1px solid white; background-color: white; object-fit: cover; object-position: center;">
+        <p><H4 class="text-white">DOST-SETUP Funding Monitoring System</H4></p>
+      </div>
+      <div>
+      <div class="overflow-y-auto">
+        <?php include("navAdmin.php"); ?>
+      </div>
+      <main class="scrollable-main">
+      </main>
+      </div>
+  </div>
+</body>
+</html>
